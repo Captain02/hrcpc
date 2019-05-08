e98bfa8548b406b4bdb0823fa95f8bb9a828458a
heidongxue123 <2960867078@qq.com>
2019-05-08 21:44:01 +0800

    相册界面布局
    相册界面布局

--- src/views/picture/add-picture.vue
+++ src/views/picture/add-picture.vue
@@ -1,10 +1,26 @@
 <template>
   <div class="app-container">
+    <h1 class="page-title">
+      <span style="font-size:18px;">上传相片</span>  
+      <span style=" float:right;margin-top:-14px">
+        <el-button type="primary" size="medium">欣赏相片</el-button>
+      </span>   
+    </h1>
     
   </div>
 </template>
 <script>
 export default {
-  name: 'add-picture'
+  name: 'picture-list'
 }
 </script>
+<style>
+.page-title {
+  padding: 15px 0;
+  font-size: 18px;
+  color: #464c5b;
+  font-weight: 400;
+  border-bottom: 1px solid #e5e5e5;
+}
+
+</style> 
