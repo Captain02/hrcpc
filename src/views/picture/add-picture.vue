b881485be4246f0e9f39b38cfc7cc0526793bc34
EchoesR <38370051+EchoesR@users.noreply.github.com>
2019-05-08 23:21:27 +0800

    相册界面布局
    相册界面布局

--- src/views/picture/add-picture.vue
+++ src/views/picture/add-picture.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="app-container">
+    <h1 class="page-title">
+      <span style="font-size:18px;">上传相片</span>  
+      <span style=" float:right;margin-top:-14px">
+        <el-button type="primary" size="medium">欣赏相片</el-button>
+      </span>   
+    </h1>
     
   </div>
 </template>
--- src/views/picture/picture-list.vue
+++ src/views/picture/picture-list.vue
@@ -1,10 +1,62 @@
 <template>
-  <div class="app-container">
+<div class="app-container" >
+    <h1 class="page-title">
+      <span style="font-size:18px;">精彩相片</span>  
+      <span style=" float:right;margin-top:-14px">
+        <el-button type="primary" size="medium">全部相册</el-button>
+        <el-button type="primary" size="medium" icon="el-icon-circle-plus-outline" @click="F_Open_dialog()">上传相片</el-button>
+      <input type="file" id="btn_file" style="display:none">
+
+      </span>   
+    </h1>
+  
+    <div class="components-container board">
+      <ul style="display:flex;flex-wrap:wrap">
+        <li
+          v-for="(item,index,key) in pic_list"
+          :key="key"
+          class="aaal"
+        >
+        <div >{{item.id}}</div>
+         </li>
+         
+        
+      </ul>
+    </div>
     
+  
   </div>
+
+  
 </template>
 <script>
 export default {
-  name: 'picture-list'
+  name: 'picture-list',
+  data(){
+    return{
+        pic_list:[
+          {id:'1',name:'23'},
+           {id:'1',name:'23'},
+            {id:'1',name:'23'},
+          ],
+    }
+  
+  },
+  methods:{
+    F_Open_dialog() { 
+      document.getElementById("btn_file").click();
+  } 
+  },
 }
 </script>
+<style>
+.aaal {
+  width: 200px;
+  height: 240px;
+  background: rgb(249, 236, 216);
+  text-align: center;
+  list-style: none;
+  margin: 10px 15px;
+  border-radius: 10px;
+}
+</style>
