c88086f45c4908857eee89946b6d4316f7a12f81
zhx_hexo <zhx_silentheart@163.com>
2019-05-17 17:38:26 +0800

    相册修改
    相册底部修改，缺少滑过动画

--- src/views/picture/picture-list.vue
+++ src/views/picture/picture-list.vue
@@ -16,14 +16,17 @@
     <viewer :images="pic_list" @inited="inited" class="viewer" ref="viewer">
       <div class="components-container board">
         <ul style="display:flex;flex-wrap:wrap">
-          <li v-for="(item, index, key) in pic_list" :key="key" class="aaal">
+          <li v-for="(item, index, key) in pic_list" :key="key" class="list-item-con ">
             <i class="el-icon-error del-img" @click="delImg(item.id)"></i>
             <div class="list-item">
               <div class="list-item-img">
                 <img :src="item.img" alt="">
               </div>
               <div class="list-img-text">
-                <span>{{item.text}}</span>
+                <p>{{item.text}}</p>
+                <p>JACK <span style="float:right">   
+                  <i class="el-icon-star-on"></i> 1</span>
+                </p>
               </div>
             </div>
           </li>
@@ -70,6 +73,9 @@ export default {
       ]
     };
   },
+  mounted: function() {
+    // animated infinite bounce 
+  },
   methods: {
     F_Open_dialog() {
       document.getElementById("btn_file").click();
@@ -106,16 +112,17 @@ export default {
   font-weight: 400;
   border-bottom: 1px solid #e5e5e5;
 }
-.aaal {
-  width: 200px;
-  height: auto;
+.list-item-con {
+  float: left;
+  height: 230px;
+  display: table;
   position: relative;
-  background: rgb(249, 236, 216);
+  background: rgb(243, 245, 246);
   text-align: center;
   list-style: none;
   margin: 10px 15px;
-  border-radius: 10px;
 }
+
 .del-img {
   position: absolute;
   color: #fff;
@@ -126,9 +133,9 @@ export default {
   cursor: pointer;
 }
 .list-item {
-  width: 100%;
-  height: 130px;
-  display: flex;
+  float: left;
+  height: 190px;
+  display: table;
   position: relative;
   flex-direction: column;
   cursor: pointer;
@@ -138,12 +145,47 @@ export default {
   height: 100px;
 }
 .list-item-img  img {
-  width: 100%;
-  height: 100%;
+  width: auto;
+  height: 170px;
 }
 .list-img-text {
   width: 100%;
-  height: 28px;
-  line-height: 28px;
+  height: 20px;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  line-height: 20px;
+  background: rgb(243, 245, 246);
+  text-align: left;
 }
+.list-img-text p:nth-child(1) {
+  color: #535351;
+}
+.list-img-text p {
+  margin: 6px 0 4px 0;
+  padding: 0 10px;
+  color: #adaeb5;
+}
+@keyframes list-item-con {
+  from {
+    -webkit-transform: scale3d(1, 1, 1);
+    transform: scale3d(1, 1, 1);
+  }
+
+  50% {
+    -webkit-transform: scale3d(1.05, 1.05, 1.05);
+    transform: scale3d(1.05, 1.05, 1.05);
+  }
+
+  to {
+    -webkit-transform: scale3d(1, 1, 1);
+    transform: scale3d(1, 1, 1);
+  }
+}
+
+.list-item-con:hover {
+  -webkit-animation-name: pulse;
+  animation-name: list-item-con;
+}
+
 </style>
