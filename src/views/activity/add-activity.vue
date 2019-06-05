5ca4083c850cc0140fb6531f84111bb7e23ec750
EchoesR <38370051+EchoesR@users.noreply.github.com>
2019-06-05 23:06:52 +0800

    上传视频
    上传视频

--- src/views/activity/add-activity.vue
+++ src/views/activity/add-activity.vue
@@ -6,6 +6,10 @@
         <el-form-item prop="name" label="活动名称：">
           <el-input v-model="activity.name" placeholder="活动名称"></el-input>
         </el-form-item>
+        <el-form-item label="活动简介：">
+          <el-input v-model="activity.profile" type="textarea"  maxlength="200" :rows="6"
+  show-word-limit placeholder="活动简介"></el-input>
+        </el-form-item>
         <el-form-item prop="actLeader" label="活动负责人：">
           <el-select
             v-model="activity.actLeader"
@@ -63,6 +67,7 @@
         <el-form-item label="图片上传：" >
           <!-- {type: 0}上传图片、{type: 1}上传视频 -->
           <el-upload
+            :disabled="!!this.imageFile"
             action="http://140.143.201.244:8081/HBO/sys/activity/uploudActivitiBananer"
             :data="{type: 0}"
             name="file"
@@ -73,17 +78,31 @@
           <el-button type="primary">上传图片</el-button>
           <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过2M</div>
           </el-upload>
-          <div class="images-view" v-show="imageFiles.length">
-            <div class="image-wrapper" v-for="(image, index) in imageFiles" :key="index">
+          <div class="images-view" v-if="imageFile">
+            <div class="image-wrapper">
               <el-image
-              :src="image.filePath"
+              :src="imageFile.filePath"
               fit="scale-down"></el-image>
-              <div class="image-text-area" :title="image.fileName">
-                <p class="image-text">{{image.fileName}}</p>
-                <icon-svg class-name="image-delicon" icon-class="close"></icon-svg>
+              <div class="image-text-area" :title="imageFile.fileName" @click="deleteImage">
+                <p class="image-text">{{imageFile.fileName}}</p><icon-svg class-name="image-delicon" icon-class="close"></icon-svg>
               </div>
             </div>
           </div>
+        </el-form-item>
+        <el-form-item label="视频上传：" >
+          <!-- {type: 0}上传图片、{type: 1}上传视频 -->
+          <el-upload
+            :disabled="!!this.videoFile"
+            action="http://140.143.201.244:8081/HBO/sys/activity/uploudActivitiBananer"
+            :data="{type: 1}"
+            name="file"
+            :show-file-list="false"
+            :on-success="handleVideoSuccess"
+            :before-upload="beforeVideoUpload"
+          >
+          <el-button type="primary">上传视频</el-button>
+          <div slot="tip" class="el-upload__tip">请上传MP4、MPEG、WMV格式或AVI格式文件，且不超过200M</div>
+          </el-upload>
         </el-form-item>
         <el-form-item label="附件上传：">
           <el-upload
@@ -96,41 +115,16 @@
             <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
           </el-upload>
         </el-form-item>
-        <el-form-item label="活动简介：">
-          <el-input v-model="activity.profile" type="textarea"  maxlength="200" :rows="6"
-  show-word-limit placeholder="活动简介"></el-input>
-        </el-form-item>
+        
         <el-form-item prop="desc" label="活动详情：">
           <mce-editor v-model="activity.descs" ></mce-editor>
         </el-form-item>
-        <!-- <el-form-item prop="collegetie" label="专业：">
-          <el-select v-model="user.collegetie" placeholder="请选择专业" class="full-width">
-            <el-option v-for="item in collegetieOptions" :key="item.id" :value="item.id" :label="item.label"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item prop="email" label="邮箱：">
-          <el-input v-model="user.email" type="email" placeholder="请输入邮箱地址"></el-input>
-        </el-form-item>
-        <el-form-item prop="mobile" label="手机号：">
-          <el-input v-model="user.mobile" placeholder="请输入手机号"></el-input>
-        </el-form-item>
-        <el-form-item prop="wechart" label="微信：">
-          <el-input v-model="user.wechart" placeholder="请输入微信号"></el-input>
-        </el-form-item>
-        <el-form-item prop="qq" label="QQ：">
-          <el-input v-model="user.qq" placeholder="请输入QQ号"></el-input>
-        </el-form-item>
-        <el-form-item prop="descs" label="自我描述：">
-          <mce-editor v-model="user.descs" ></mce-editor>
-        </el-form-item>
-        
         <el-form-item class="">
           <el-col :offset="5">
-            <el-button type="primary" @click="addUser">添加</el-button>
+            <el-button type="primary">添加</el-button>
           </el-col>
-        </el-form-item> -->
+        </el-form-item>
       </el-form>
-      <p>{{imageFiles}}</p>
       {{activity}}
     </el-card>
   </div>
@@ -138,6 +132,7 @@
 <script>
 import { getUsers as getUsersApi } from '@/api/user'
 import { getCollegeInfo as getCollegeInfoApi } from '@/api/comm'
+import { deleteFile as deleteFileApi } from '@/api/activity'
 import MceEditor from '@/components/MceEditor'
 window.tinymce.baseURL = '/static/tinymce'
 window.tinymce.suffix = '.min'
@@ -154,6 +149,7 @@ export default {
         timer: '',
         actStartTime: '',         // 活动开始时间
         actEndTime: '',           // 活动结束时间
+        images: '',               // 上传图片后获取的id
         croWdPeople: [],          // 活动面向人群
         profile: '',              // 活动简介
         desc: '',                 // 活动详情
@@ -162,7 +158,8 @@ export default {
       },
       enclosureList: [],               // 附件文件
       processState: '',              // 输入的流程信息
-      imageFiles: [],
+      imageFile: null,                 // 上传的图片海报
+      videoFile: null,
       usersOptions: [],             // 活动负责人选项
       collegeOptinos: [],           // 面向人群选项
       rules: {}
@@ -177,28 +174,58 @@ export default {
       this.activity.processNodes.splice(index, 1)
     },
     beforeImageUpload(file) {
-      console.log('file', file)
       let typeWhiteList = ['image/jpeg', 'image/png']
       const isJPG = typeWhiteList.includes(file.type)
-      const isLt2M = file.size / 1024 / 1024 < 2
+      const isLt2M = file.size / 1024 / 1024 < 5
 
       if (!isJPG) {
-        this.$message.error('上传头像图片只能是 JPG 格式或PNG格式!')
+        this.$message.error('上传图片只能是 JPG 格式或PNG格式!')
       }
       if (!isLt2M) {
-        this.$message.error('上传头像图片大小不能超过 2MB!')
+        this.$message.error('上传图片大小不能超过 2MB!')
       }
       return isJPG && isLt2M
     },
+    beforeVideoUpload(file) {
+      console.log(file)
+      let typeWhiteList = ['video/mp4', 'video/avi', 'video/mpeg', 'video/x-ms-wmv', '']
+      const isType = typeWhiteList.includes(file.type)
+      const isSize = file.size / 1024 < 200
+
+      if (!isType) {
+        this.$message.error('上传视频只能是 MP4、MPEG、WMV格式或AVI格式!')
+      }
+      if (!isSize) {
+        this.$message.error('上传视频大小不能超过 200MB!')
+      }
+      return isType && isSize
+      return false
+    },
     handleImageSuccess(res, file) {
-      console.log(res, file)
       if(res.code !== 0){
         this.$message.error('上传失败，请重新上传!')
         return
       }
       let { data } = res
-      this.imageFiles.push({ fileName: data.fileName, filePath: data.filePath, id: data.id })
+      this.imageFile = {
+        fileName: data.fileName, 
+        filePath: data.filePath, 
+        id: data.id
+      }
+      this.activity.images = data.id
+    },
+    handleVideoSuccess(res, file) {
       
+    },
+    deleteImage(event) {
+      let tag = event.target || event.srcElement
+      let tagName = tag.tagName.toLowerCase()
+      if(tagName === 'svg' || tagName === 'use'){
+        deleteFileApi(this.imageFile.id, this.imageFile.filePath).then((result) => {
+          this.imageFile = null
+          this.activity.images = ''
+        })
+      }
     },
     handleSearch(val) {
       getUsersApi({name: val, page: 1, size: 9999, username: ''}).then((result) => {
@@ -264,6 +291,9 @@ export default {
           white-space: nowrap;
           display: inline-block;
           vertical-align: middle;
+        }
+        .image-delicon {
+          width: 20%;
         }
       }
     }
