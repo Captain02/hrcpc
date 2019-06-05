30dd5b8a11869123cbe6bf6eff51f30a157a3586
EchoesR <38370051+EchoesR@users.noreply.github.com>
2019-06-05 19:38:30 +0800

    活动图片
    活动图片

--- src/views/activity/add-activity.vue
+++ src/views/activity/add-activity.vue
@@ -34,14 +34,17 @@
           </el-date-picker>
         </el-form-item>
         <el-form-item prop="croWdPeople" label="面向人群：">
-          <el-select v-model="activity.croWdPeople" placeholder="面向人群" multiple clearable class="half-width">
+          <el-select v-model="activity.croWdPeople" placeholder="面向人群" multiple clearable class="full-width">
             <el-option v-for="college in collegeOptinos" :key="college.id" :label="college.value" :value="college.id"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="活动流程：">
+          
           <div class="activity-process-wrapper">
-            <el-steps :active="activity.processNodes.length" space="20%">
-              <el-step v-for="(state, index) in activity.processNodes" :title="state" :key="index"></el-step>
+            <el-steps :active="activity.processNodes.length" space="10%">
+              <el-step v-for="(state, index) in activity.processNodes" :key="index">
+                <el-button slot="title" @click="deleteProcess(index)" type="text">{{state}}</el-button>
+              </el-step>
             </el-steps>
           </div>
           <el-popover
@@ -50,14 +53,48 @@
             width="300"
             trigger="click">
             <el-row :gutter="10">
-              <el-col :span="19"><el-input></el-input></el-col>
-              <el-col :span="5"><el-button>确定</el-button></el-col>
+              <el-col :span="19"><el-input v-model="processState"></el-input></el-col>
+              <el-col :span="5"><el-button @click="addProcess">确定</el-button></el-col>
             </el-row>
-            
-            
             <el-button slot="reference" type="primary" icon="el-icon-plus" circle></el-button>
           </el-popover>
           
+        </el-form-item>
+        <el-form-item label="图片上传：" >
+          <!-- {type: 0}上传图片、{type: 1}上传视频 -->
+          <el-upload
+            action="http://140.143.201.244:8081/HBO/sys/activity/uploudActivitiBananer"
+            :data="{type: 0}"
+            name="file"
+            :show-file-list="false"
+            :on-success="handleImageSuccess"
+            :before-upload="beforeImageUpload"
+          >
+          <el-button type="primary">上传图片</el-button>
+          <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过2M</div>
+          </el-upload>
+          <div class="images-view" v-show="imageFiles.length">
+            <div class="image-wrapper" v-for="(image, index) in imageFiles" :key="index">
+              <el-image
+              :src="image.filePath"
+              fit="scale-down"></el-image>
+              <div class="image-text-area" :title="image.fileName">
+                <p class="image-text">{{image.fileName}}</p>
+                <icon-svg class-name="image-delicon" icon-class="close"></icon-svg>
+              </div>
+            </div>
+          </div>
+        </el-form-item>
+        <el-form-item label="附件上传：">
+          <el-upload
+            ref="upload"
+            action="https://jsonplaceholder.typicode.com/posts/"
+    
+            :file-list="enclosureList"
+            :auto-upload="false">
+            <el-button slot="trigger" type="primary">选取文件</el-button>
+            <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+          </el-upload>
         </el-form-item>
         <el-form-item label="活动简介：">
           <el-input v-model="activity.profile" type="textarea"  maxlength="200" :rows="6"
@@ -93,6 +130,7 @@
           </el-col>
         </el-form-item> -->
       </el-form>
+      <p>{{imageFiles}}</p>
       {{activity}}
     </el-card>
   </div>
@@ -122,12 +160,46 @@ export default {
         processNodes: [],         // 活动流程
 
       },
-      usersOptions: [],
-      collegeOptinos: [],
+      enclosureList: [],               // 附件文件
+      processState: '',              // 输入的流程信息
+      imageFiles: [],
+      usersOptions: [],             // 活动负责人选项
+      collegeOptinos: [],           // 面向人群选项
       rules: {}
     }
   },
   methods: {
+    addProcess() {
+      this.activity.processNodes.push(this.processState)
+      this.processState = ''
+    },
+    deleteProcess(index) {
+      this.activity.processNodes.splice(index, 1)
+    },
+    beforeImageUpload(file) {
+      console.log('file', file)
+      let typeWhiteList = ['image/jpeg', 'image/png']
+      const isJPG = typeWhiteList.includes(file.type)
+      const isLt2M = file.size / 1024 / 1024 < 2
+
+      if (!isJPG) {
+        this.$message.error('上传头像图片只能是 JPG 格式或PNG格式!')
+      }
+      if (!isLt2M) {
+        this.$message.error('上传头像图片大小不能超过 2MB!')
+      }
+      return isJPG && isLt2M
+    },
+    handleImageSuccess(res, file) {
+      console.log(res, file)
+      if(res.code !== 0){
+        this.$message.error('上传失败，请重新上传!')
+        return
+      }
+      let { data } = res
+      this.imageFiles.push({ fileName: data.fileName, filePath: data.filePath, id: data.id })
+      
+    },
     handleSearch(val) {
       getUsersApi({name: val, page: 1, size: 9999, username: ''}).then((result) => {
         let { data } = result
@@ -157,8 +229,45 @@ export default {
   .el-date-editor {
     width: 50%;
   }
-  // .activity-process-wrapper {
-
-  // }
+  .activity-process-wrapper {
+    min-height: 65px;
+  }
+  .images-view {
+    padding: 5px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    .image-wrapper {
+      position: relative;
+      display: inline-block;
+      width: 120px;
+      height: 100px;
+      margin-right: 10px;
+      overflow: hidden;
+      &:hover .image-text-area{
+        bottom: 0;
+      }
+      .image-text-area {
+        position: absolute;
+        width: 100%;
+        bottom: -25px;
+        cursor: pointer;
+        transition: all .3s;
+        line-height: 20px;
+        background: rgba(0, 0, 0, .3);
+        color: #fff;
+       
+        .image-text {
+          width: 80%;
+          overflow: hidden;
+          font-size: 12px;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+          display: inline-block;
+          vertical-align: middle;
+        }
+      }
+    }
+  }
+ 
 }
 </style>
