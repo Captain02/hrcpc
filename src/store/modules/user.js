a10828849ee21855be7027de5b0e1efe193c0933
EchoesR <38370051+EchoesR@users.noreply.github.com>
2019-08-24 20:22:34 +0800

    个人中心
    个人中心

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -40,6 +40,7 @@ const user = {
       state.permits = permits
     },
     SET_AVATAR(state, avatar) {
+      console.log('mutiation', avatar)
       state.avatar = avatar
     },
     SET_GENDER(state, gender) {
